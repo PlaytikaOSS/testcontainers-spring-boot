b3c8e25c62da6f5f4fc32cc5b440808033be8a00
Matvey Maslov <mmaslov@playtika.com>
2019-02-20 15:11:09 +0200

    Redis cluster

--- embedded-redis/src/main/java/com/playtika/test/redis/EmbeddedRedisBootstrapConfiguration.java
+++ embedded-redis/src/main/java/com/playtika/test/redis/EmbeddedRedisBootstrapConfiguration.java
@@ -25,40 +25,42 @@ package com.playtika.test.redis;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.condition.AllNestedConditions;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
 import org.testcontainers.containers.GenericContainer;
 
-import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static com.playtika.test.common.utils.ContainerUtils.containerLogsConsumer;
+import static com.playtika.test.redis.EnvUtils.registerRedisEnvironment;
 import static com.playtika.test.redis.RedisProperties.BEAN_NAME_EMBEDDED_REDIS;
 
 @Slf4j
 @Configuration
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
-@ConditionalOnProperty(name = "embedded.redis.enabled", matchIfMissing = true)
+@Conditional(EmbeddedRedisBootstrapConfiguration.EmbeddedRedisCondition.class)
 @EnableConfigurationProperties(RedisProperties.class)
 public class EmbeddedRedisBootstrapConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    RedisStatusCheck redisStartupCheckStrategy(RedisProperties properties) {
+    RedisStatusCheck redisStartupCheckStrategy() {
         return new RedisStatusCheck();
     }
 
     @Bean(name = BEAN_NAME_EMBEDDED_REDIS, destroyMethod = "stop")
     public GenericContainer redis(ConfigurableEnvironment environment,
                                   RedisProperties properties,
-                                  RedisStatusCheck redisStatusCheck) throws Exception {
+                                  RedisStatusCheck redisStatusCheck) {
 
-        log.info("Starting redis server. Docker image: {}", properties.dockerImage);
+        log.info("Starting Redis server. Docker image: {}", properties.dockerImage);
 
         GenericContainer redis =
                 new GenericContainer(properties.dockerImage)
@@ -70,22 +72,23 @@ public class EmbeddedRedisBootstrapConfiguration {
                         .waitingFor(redisStatusCheck)
                         .withStartupTimeout(properties.getTimeoutDuration());
         redis.start();
-        registerRedisEnvironment(redis, environment, properties);
+        Map<String, Object> redisEnv = registerRedisEnvironment(environment, redis, properties, redis.getMappedPort(properties.port));
+        log.info("Started Redis server. Connection details: {}", redisEnv);
         return redis;
     }
 
-    private void registerRedisEnvironment(GenericContainer redis,
-                                          ConfigurableEnvironment environment,
-                                          RedisProperties properties) {
-        Integer mappedPort = redis.getMappedPort(properties.port);
-        String host = redis.getContainerIpAddress();
+    static class EmbeddedRedisCondition extends AllNestedConditions {
 
-        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
-        map.put("embedded.redis.port", mappedPort);
-        map.put("embedded.redis.host", host);
-        map.put("embedded.redis.password", properties.getPassword());
-        map.put("embedded.redis.user", properties.getUser());
-        MapPropertySource propertySource = new MapPropertySource("embeddedRedisInfo", map);
-        environment.getPropertySources().addFirst(propertySource);
+        public EmbeddedRedisCondition() {
+            super(ConfigurationPhase.REGISTER_BEAN);
+        }
+
+        @ConditionalOnProperty(name = "embedded.redis.enabled", matchIfMissing = true)
+        static class EmbeddedRedisEnabled {
+        }
+
+        @ConditionalOnProperty(name = "embedded.redis.clustered", havingValue = "false", matchIfMissing = true)
+        static class EmbeddedRedisNotClustered {
+        }
     }
 }
---
+++ embedded-redis/src/main/java/com/playtika/test/redis/RedisClusterStatusCheck.java
@@ -0,0 +1,75 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Playtika
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.playtika.test.redis;
+
+import com.google.common.base.Joiner;
+import com.playtika.test.common.checks.AbstractRetryingWaitStrategy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.testcontainers.containers.ContainerLaunchException;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RedisClusterStatusCheck extends AbstractRetryingWaitStrategy {
+
+    private final RedisProperties properties;
+
+    @Override
+    protected void waitUntilReady() {
+        try {
+            super.waitUntilReady();
+        } catch (ContainerLaunchException e) {
+            logClusterInfo();
+            throw e;
+        }
+    }
+
+    @Override
+    protected boolean isReady() {
+        try (Jedis jedis = new Jedis(properties.host, properties.port)) {
+            jedis.auth(properties.password);
+            String clusterInfo = jedis.clusterInfo();
+            return clusterInfo.contains("cluster_state:ok");
+        }
+    }
+
+    private void logClusterInfo() {
+        try (Jedis jedis = new Jedis(properties.host, properties.port)) {
+            jedis.auth(properties.password);
+            String clusterInfo = jedis.clusterInfo();
+            String info = jedis.info();
+            List<String> config = jedis.configGet("*");
+            String clusterNodes = jedis.clusterNodes();
+            log.error("Cluster in failed state:\n" +
+                            "-- cluster info:\n{}\n" +
+                            "-- nodes:\n{}\n",
+                            "-- info:\n{}\n" +
+                            "-- config:\n{}" +
+                            clusterInfo, clusterNodes, info, Joiner.on("\n").join(config));
+        }
+    }
+}
