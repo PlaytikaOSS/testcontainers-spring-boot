e202cd504f85a8eae77f25ee2e84a7af2ab33c30
Jochen Christ <jochen.christ@innoq.com>
2019-09-26 22:59:18 +0200

    #101 Pin minio docker image to current version. (still expecting to fail)

--- embedded-minio/src/main/java/com/playtika/test/minio/MinioProperties.java
+++ embedded-minio/src/main/java/com/playtika/test/minio/MinioProperties.java
@@ -37,7 +37,7 @@ public class MinioProperties extends CommonContainerProperties {
     static final String MINIO_BEAN_NAME = "minio";
 
     boolean enabled = true;
-    String dockerImage = "minio/minio";
+    String dockerImage = "minio/minio:RELEASE.2019-09-26T19-42-35Z";
     String accessKey = "AKIAIOSFODNN7EXAMPLE";
     String secretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
     String userName = "minio";
