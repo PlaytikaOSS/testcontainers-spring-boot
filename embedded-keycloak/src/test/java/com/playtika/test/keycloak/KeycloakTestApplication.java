3ab98a25b321896865cc603071ca5d575a3ec764
Holger Stolzenberg <h.stolzenberg@ewerk.com>
2020-01-29 16:20:17 +0100

    Remove unused AtEnableWebMVC annotation

--- embedded-keycloak/src/test/java/com/playtika/test/keycloak/KeycloakTestApplication.java
+++ embedded-keycloak/src/test/java/com/playtika/test/keycloak/KeycloakTestApplication.java
@@ -4,9 +4,7 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @KeycloakConfiguration
 @SpringBootApplication
 public class KeycloakTestApplication {
