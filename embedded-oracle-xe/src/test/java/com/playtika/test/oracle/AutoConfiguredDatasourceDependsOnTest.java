4eb87774413619338815cc39afe1cca859df908e
alex <apakhomov@gridfore.com>
2020-05-01 19:34:49 +0300

    Optimize imports

--- embedded-oracle-xe/src/test/java/com/playtika/test/oracle/AutoConfiguredDatasourceDependsOnTest.java
+++ embedded-oracle-xe/src/test/java/com/playtika/test/oracle/AutoConfiguredDatasourceDependsOnTest.java
@@ -29,7 +29,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.sql.DataSource;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
