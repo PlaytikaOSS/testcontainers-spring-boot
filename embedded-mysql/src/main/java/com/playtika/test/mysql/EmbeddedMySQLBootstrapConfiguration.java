961b3253cd37cb9b2756d484338896f2c7d5b0bc
Ivan Vasyliev <vasilievip@gmail.com>
2020-05-03 18:55:17 +0300

    Update EmbeddedMySQLBootstrapConfiguration.java

--- embedded-mysql/src/main/java/com/playtika/test/mysql/EmbeddedMySQLBootstrapConfiguration.java
+++ embedded-mysql/src/main/java/com/playtika/test/mysql/EmbeddedMySQLBootstrapConfiguration.java
@@ -27,7 +27,6 @@ import java.util.LinkedHashMap;
 
 import com.github.dockerjava.api.model.Capability;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
