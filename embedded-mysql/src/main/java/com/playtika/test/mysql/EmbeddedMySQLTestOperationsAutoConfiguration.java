e338909c52da272fa409ab5ef938fe654e70f974
Mingyuan Wu <my.wu@outlook.com>
2020-05-03 22:55:42 +0800

    add mysql module

---
+++ embedded-mysql/src/main/java/com/playtika/test/mysql/EmbeddedMySQLTestOperationsAutoConfiguration.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Playtika
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.playtika.test.mysql;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.MySQLContainer;
+
+import com.playtika.test.common.operations.DefaultNetworkTestOperations;
+import com.playtika.test.common.operations.NetworkTestOperations;
+import com.playtika.test.common.properties.InstallPackageProperties;
+import com.playtika.test.common.utils.AptGetPackageInstaller;
+import com.playtika.test.common.utils.PackageInstaller;
+
+import static com.playtika.test.mysql.MySQLProperties.BEAN_NAME_EMBEDDED_MYSQL;
+
+@Configuration
+@ConditionalOnBean({MySQLProperties.class})
+@ConditionalOnProperty(value = "embedded.mysql.enabled", matchIfMissing = true)
+public class EmbeddedMySQLTestOperationsAutoConfiguration {
+
+    @Bean
+    @ConfigurationProperties("embedded.mysql.install")
+    public InstallPackageProperties mysqlPackageProperties() {
+        InstallPackageProperties properties = new InstallPackageProperties();
+        properties.setPackages(Collections.singleton("iproute2"));// we need iproute2 for tc command to work
+        return properties;
+    }
+
+    @Bean
+    public PackageInstaller mysqlPackageInstaller(
+            InstallPackageProperties mysqlPackageProperties,
+            @Qualifier(BEAN_NAME_EMBEDDED_MYSQL) MySQLContainer mysql
+    ) {
+        return new AptGetPackageInstaller(mysqlPackageProperties, mysql);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "mysqlNetworkTestOperations")
+    public NetworkTestOperations mysqlNetworkTestOperations(
+            @Qualifier(BEAN_NAME_EMBEDDED_MYSQL) MySQLContainer mysql
+    ) {
+        return new DefaultNetworkTestOperations(mysql);
+    }
+}
