e2b2c03b782e59a62c92806c598390efea4dd21f
Taras Danylchuk <tdanylchuk@playtika.com>
2020-05-07 15:38:52 -0400

    - removed redundant tests (#288)
    - sets postgres 12 as default image
    
    fixes https://github.com/testcontainers/testcontainers-spring-boot/issues/286

--- embedded-postgresql/src/test/java/com/playtika/test/postgresql/DisablePostgreSQLTest.java
+++ embedded-postgresql/src/test/java/com/playtika/test/postgresql/DisablePostgreSQLTest.java
@@ -23,21 +23,19 @@
  */
 package com.playtika.test.postgresql;
 
+import com.playtika.test.postgresql.dummyapp.TestApplication;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.GenericContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles("disabled")
-@SpringBootTest(properties = "embedded.postgresql.enabled=false")
+@SpringBootTest(classes = TestApplication.class, properties = "embedded.postgresql.enabled=false")
 class DisablePostgreSQLTest {
 
     @Autowired
