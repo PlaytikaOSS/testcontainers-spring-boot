f341e8a4a8c940ca3d390d877c22d01eda169a09
alexromanov <al8x.romanov@gmail.com>
2020-05-10 15:02:20 +0300

    [248] Add embedded localstack configuration

---
+++ embedded-localstack/src/main/java/com/playtika/test/localstack/LocalStackProperties.java
@@ -0,0 +1,24 @@
+package com.playtika.test.localstack;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import com.playtika.test.common.properties.CommonContainerProperties;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ConfigurationProperties("embedded.localstack")
+public class LocalStackProperties extends CommonContainerProperties {
+    public static final String BEAN_NAME_EMBEDDED_LOCALSTACK = "embeddedLocalstack";
+    public Collection<String> services = Collections.emptyList();
+    public int edgePort = 4566;
+    public String defaultRegion = "us-east-1";
+    public String hostname = "localhost";
+    public String hostnameExternal = "localhost";
+    public String dockerImage = "localstack/localstack:0.10.8";
+    public boolean useSsl = false;
+}
