1d739476a3c8389476fa0cf18a72be73181f32ea
ivasylyev <ivasylyev@playtika.com>
2020-05-26 21:48:24 +0300

    #312 fixed static analysis issue

--- testcontainers-common/src/main/java/com/playtika/test/common/spring/DockerPresenceMarker.java
+++ testcontainers-common/src/main/java/com/playtika/test/common/spring/DockerPresenceMarker.java
@@ -28,12 +28,12 @@ import lombok.Value;
 @Value
 public class DockerPresenceMarker {
 
+    boolean dockerPresent;
+
     public DockerPresenceMarker(boolean dockerPresent) {
         if(!dockerPresent){
             throw new DockerNotPresentException("Docker must be present in order for testcontainers to work properly!");
         }
         this.dockerPresent = dockerPresent;
     }
-
-    boolean dockerPresent;
 }
