a00209e724a71580029227c74777cb35fbcae250
ivasylyev <ivasylyev@playtika.com>
2020-06-19 11:05:06 +0300

    #356 code review comments

--- embedded-elasticsearch/src/main/java/com/playtika/test/elasticsearch/EmbeddedElasticSearchTestOperationsAutoConfiguration.java
+++ embedded-elasticsearch/src/main/java/com/playtika/test/elasticsearch/EmbeddedElasticSearchTestOperationsAutoConfiguration.java
@@ -26,11 +26,10 @@ package com.playtika.test.elasticsearch;
 import com.playtika.test.common.operations.DefaultNetworkTestOperations;
 import com.playtika.test.common.operations.NetworkTestOperations;
 import com.playtika.test.common.properties.InstallPackageProperties;
-import com.playtika.test.common.spring.DockerPresenceMarker;
 import com.playtika.test.common.utils.PackageInstaller;
 import com.playtika.test.common.utils.YumPackageInstaller;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -42,7 +41,7 @@ import java.util.Collections;
 import static com.playtika.test.elasticsearch.ElasticSearchProperties.BEAN_NAME_EMBEDDED_ELASTIC_SEARCH;
 
 @Configuration
-@ConditionalOnBean({DockerPresenceMarker.class})
+@ConditionalOnExpression("${embedded.containers.enabled:true}")
 @ConditionalOnProperty(value = "embedded.elasticsearch.install.enabled")
 public class EmbeddedElasticSearchTestOperationsAutoConfiguration {
 
@@ -63,7 +62,7 @@ public class EmbeddedElasticSearchTestOperationsAutoConfiguration {
 
     @Bean
     public NetworkTestOperations elasticSearchNetworkTestOperations(@Qualifier(BEAN_NAME_EMBEDDED_ELASTIC_SEARCH)
-                                                                                ElasticsearchContainer elasticSearch) {
+                                                                            ElasticsearchContainer elasticSearch) {
         return new DefaultNetworkTestOperations(elasticSearch);
     }
 }
