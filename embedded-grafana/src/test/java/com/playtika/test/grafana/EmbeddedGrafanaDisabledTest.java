2f13a0bb49c9181f0e9202a23b18937e1cd60391
Олександр Геращенко <16224854+olexandrger@users.noreply.github.com>
2021-06-02 16:37:59 +0300

    embedded grafana module (#667)
    Co-authored-by: oherashchenko <oherashchenko@playtika.com>

---
+++ embedded-grafana/src/test/java/com/playtika/test/grafana/EmbeddedGrafanaDisabledTest.java
@@ -0,0 +1,34 @@
+package com.playtika.test.grafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = EmbeddedGrafanaDisabledTest.TestConfiguration.class,
+        properties = "embedded.grafana.enabled=false"
+)
+public class EmbeddedGrafanaDisabledTest {
+
+    @Autowired
+    ConfigurableListableBeanFactory beanFactory;
+
+    @Test
+    public void contextLoads() {
+        String[] containers = BeanFactoryUtils.beanNamesForTypeIncludingAncestors(beanFactory, GenericContainer.class);
+        assertThat(containers).isEmpty();
+    }
+
+    @EnableAutoConfiguration
+    @Configuration
+    static class TestConfiguration {
+
+    }
+}
