370948add391a835f48c3410e31ea92eb7fc72a1
Ильиных Илья Сергеевич <ilyasyoy@gmail.com>
2021-08-16 19:46:27 +0300

    Initial implementation of DB2 from issue #280

---
+++ embedded-db2/src/main/java/com/playtika/test/db2/EmbeddedDb2BootstrapConfiguration.java
@@ -0,0 +1,102 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Playtika
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.playtika.test.db2;
+
+import com.playtika.test.common.spring.DockerPresenceBootstrapConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.testcontainers.containers.Db2Container;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
+
+import java.util.LinkedHashMap;
+
+import static com.playtika.test.common.utils.ContainerUtils.configureCommonsAndStart;
+import static org.testcontainers.shaded.com.google.common.base.Strings.isNullOrEmpty;
+
+@Slf4j
+@Configuration
+@ConditionalOnExpression("${embedded.containers.enabled:true}")
+@AutoConfigureAfter(DockerPresenceBootstrapConfiguration.class)
+@ConditionalOnProperty(name = "embedded.db2.enabled", matchIfMissing = true)
+@EnableConfigurationProperties(Db2Properties.class)
+public class EmbeddedDb2BootstrapConfiguration {
+
+    @Bean(name = Db2Properties.BEAN_NAME_EMBEDDED_DB2, destroyMethod = "stop")
+    public Db2Container db2(ConfigurableEnvironment environment,
+                            Db2Properties properties) {
+        log.info("Starting db2 server. Docker image: {}", properties.getDockerImage());
+
+        Db2Container db2Container = new Db2Container(properties.getDockerImage())
+                .withDatabaseName(properties.getDatabase())
+                .withUsername(properties.getUser())
+                .withPassword(properties.getPassword())
+                .withInitScript(properties.getInitScriptPath());
+
+        String startupLogCheckRegex = properties.getStartupLogCheckRegex();
+        if (!isNullOrEmpty(startupLogCheckRegex)) {
+            WaitStrategy waitStrategy = new LogMessageWaitStrategy()
+                    .withRegEx(startupLogCheckRegex);
+            db2Container.setWaitStrategy(waitStrategy);
+        }
+
+        if (properties.isAcceptLicence()) {
+            db2Container.acceptLicense();
+        }
+
+        db2Container = (Db2Container) configureCommonsAndStart(db2Container, properties, log);
+        registerDb2Environment(db2Container, environment, properties);
+
+        return db2Container;
+    }
+
+    private void registerDb2Environment(Db2Container db2Container,
+                                        ConfigurableEnvironment environment,
+                                        Db2Properties properties) {
+        Integer mappedPort = db2Container.getMappedPort(Db2Container.DB2_PORT);
+        String host = db2Container.getContainerIpAddress();
+
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("embedded.db2.port", mappedPort);
+        map.put("embedded.db2.host", host);
+        map.put("embedded.db2.database", properties.getDatabase());
+        map.put("embedded.db2.user", properties.getUser());
+        map.put("embedded.db2.password", properties.getPassword());
+
+        String jdbcURL = "jdbc:db2://{}:{}/{}";
+        log.info("Started db2 server. Connection details: {}, " +
+                "JDBC connection url: " + jdbcURL, map, host, mappedPort, properties.getDatabase());
+
+        MapPropertySource propertySource = new MapPropertySource("embeddedDb2Info", map);
+        environment.getPropertySources().addFirst(propertySource);
+    }
+
+}
