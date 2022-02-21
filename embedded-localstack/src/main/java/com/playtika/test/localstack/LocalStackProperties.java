829f8f661b81da3a7c362cff17fafe3f5eed53d4
asmirnova <asmirnova@playtika.com>
2022-02-21 19:16:25 +0200

    Update localstack to 0.14.0. Remove deprecated properties (single region and use ssl) (https://docs.localstack.cloud/localstack/configuration/#deprecated)

--- embedded-localstack/src/main/java/com/playtika/test/localstack/LocalStackProperties.java
+++ embedded-localstack/src/main/java/com/playtika/test/localstack/LocalStackProperties.java
@@ -16,10 +16,8 @@ public class LocalStackProperties extends CommonContainerProperties {
     public static final String BEAN_NAME_EMBEDDED_LOCALSTACK = "embeddedLocalstack";
     public Collection<LocalStackContainer.Service> services = Collections.emptyList();
     public int edgePort = 4566;
-    public String defaultRegion = "us-east-1";
     public String hostname = "localhost";
     public String hostnameExternal = "localhost";
     // https://hub.docker.com/r/localstack/localstack
-    public String dockerImage = "localstack/localstack:0.12.16";
-    public boolean useSsl = false;
+    public String dockerImage = "localstack/localstack:0.14.0";
 }
