e2b30d8eae861d24314bb5e290f857888aa8fcf0
Anastasiia Smirnova <zminyty@gmail.com>
2022-03-18 22:20:57 +0100

    Toxiproxy support

---
+++ embedded-toxiproxy/src/main/java/com/playtika/test/toxiproxy/ToxiProxyProperties.java
@@ -0,0 +1,22 @@
+package com.playtika.test.toxiproxy;
+
+import com.playtika.test.common.properties.CommonContainerProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ConfigurationProperties("embedded.toxi-proxy")
+public class ToxiProxyProperties extends CommonContainerProperties {
+
+    public ToxiProxyProperties() {
+        //we override here the default one that is used by org.testcontainers:toxiproxy module
+        this.setDockerImage("ghcr.io/shopify/toxiproxy:2.4.0");
+    }
+
+    @Override
+    public String getDefaultDockerImage() {
+        return "shopify/toxiproxy";
+    }
+}
