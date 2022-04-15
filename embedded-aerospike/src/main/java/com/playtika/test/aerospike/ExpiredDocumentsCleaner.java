baa90833211879fa707a1e5a1e0763304730b5e7
admitrov <admitrov@playtika.com>
2022-04-15 17:08:33 +0200

    Aerospike without lua script

--- embedded-aerospike/src/main/java/com/playtika/test/aerospike/ExpiredDocumentsCleaner.java
+++ embedded-aerospike/src/main/java/com/playtika/test/aerospike/ExpiredDocumentsCleaner.java
@@ -1,24 +1,13 @@
 package com.playtika.test.aerospike;
 
 import com.aerospike.client.AerospikeClient;
-import com.aerospike.client.Language;
-import com.aerospike.client.Value;
-import com.aerospike.client.query.Statement;
-import com.aerospike.client.task.ExecuteTask;
-import com.aerospike.client.task.RegisterTask;
 import org.springframework.util.Assert;
 
 import java.time.Instant;
-import java.util.concurrent.TimeUnit;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ExpiredDocumentsCleaner {
-
-    private static final String PACKAGE_NAME = "remove_expired";
-    private static final String FUNC_NAME = "remove_expired";
-    private static final String RESOURCE_PATH = "udf/remove_expired.lua";
-    private static final String SERVER_PATH = "remove_expired.lua";
-    private static final int SLEEP_INTERVAL = 100;
-    private static final int TIMEOUT = 10_000;
 
     private final AerospikeClient client;
     private final String namespace;
@@ -28,14 +17,6 @@ public class ExpiredDocumentsCleaner {
         Assert.notNull(namespace, "Namespace can not be null");
         this.client = client;
         this.namespace = namespace;
-
-        registerUdf();
-    }
-
-    private void registerUdf() {
-        ClassLoader classLoader = ExpiredDocumentsCleaner.class.getClassLoader();
-        RegisterTask registerTask = client.register(null, classLoader, RESOURCE_PATH, SERVER_PATH, Language.LUA);
-        registerTask.waitTillComplete(SLEEP_INTERVAL, TIMEOUT);
     }
 
     public void cleanExpiredDocumentsBefore(Instant expireTime) {
@@ -43,14 +24,8 @@ public class ExpiredDocumentsCleaner {
     }
 
     public void cleanExpiredDocumentsBefore(long expireTimeMillis) {
-        long duration = expireTimeMillis - System.currentTimeMillis();
-        int expiration = (int) TimeUnit.MILLISECONDS.toSeconds(duration);
-        Value value = Value.get(expiration);
-
-        Statement statement = new Statement();
-        statement.setNamespace(namespace);
-
-        ExecuteTask executeTask = client.execute(null, statement, PACKAGE_NAME, FUNC_NAME, value);
-        executeTask.waitTillComplete(SLEEP_INTERVAL, TIMEOUT);
+        Calendar beforeLastUpdate = Calendar.getInstance();
+        beforeLastUpdate.setTime(new Date(expireTimeMillis));
+        client.truncate(null, namespace, null, beforeLastUpdate);
     }
 }
--- embedded-aerospike/src/main/resources/udf/remove_expired.lua
+++
@@ -1,6 +0,0 @@
-function remove_expired(rec, expireTime)
-    local recordTtl = record.ttl(rec)
-    if (recordTtl > 0 and recordTtl < expireTime) then
-        aerospike:remove(rec)
-    end
-end
