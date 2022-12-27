056ebda976650a5f17a5e0fddc75d3eef33f932d
Thomas Hammerl <thomas.hammerl@semanticlabs.at>
2022-12-27 15:12:07 +0100

    #1054 Use @ImportAutoConfiguration for Camel and Google PubSub
     - So that auto-configurations are also loaded with Spring Boot 3.
     - Both libraries are using the spring.factories file to register their auto-configurations which is not supported anymore in Spring Boot 3. See https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.0-Migration-Guide#auto-configuration-files.

--- embedded-google-pubsub/src/main/java/com/playtika/test/pubsub/EmbeddedPubsubDependenciesAutoConfiguration.java
+++ embedded-google-pubsub/src/main/java/com/playtika/test/pubsub/EmbeddedPubsubDependenciesAutoConfiguration.java
@@ -1,10 +1,14 @@
 package com.playtika.test.pubsub;
 
+import com.google.cloud.spring.autoconfigure.core.GcpContextAutoConfiguration;
+import com.google.cloud.spring.autoconfigure.pubsub.GcpPubSubAutoConfiguration;
+import com.google.cloud.spring.autoconfigure.pubsub.GcpPubSubEmulatorAutoConfiguration;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.playtika.test.common.spring.DependsOnPostProcessor;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -13,6 +17,7 @@ import org.springframework.context.annotation.Bean;
 import static com.playtika.test.pubsub.PubsubProperties.BEAN_NAME_EMBEDDED_GOOGLE_PUBSUB;
 
 @AutoConfiguration
+@ImportAutoConfiguration(classes = {GcpContextAutoConfiguration.class, GcpPubSubEmulatorAutoConfiguration.class, GcpPubSubAutoConfiguration.class})
 @AutoConfigureOrder
 @ConditionalOnExpression("${embedded.containers.enabled:true}")
 @ConditionalOnClass(PubSubTemplate.class)
--- embedded-kafka/src/main/java/com/playtika/test/kafka/configuration/camel/EmbeddedKafkaCamelAutoConfiguration.java
+++ embedded-kafka/src/main/java/com/playtika/test/kafka/configuration/camel/EmbeddedKafkaCamelAutoConfiguration.java
@@ -2,9 +2,11 @@ package com.playtika.test.kafka.configuration.camel;
 
 import com.playtika.test.common.spring.DependsOnPostProcessor;
 import org.apache.camel.CamelContext;
+import org.apache.camel.spring.boot.CamelAutoConfiguration;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.Bean;
 import static com.playtika.test.kafka.properties.KafkaConfigurationProperties.KAFKA_BEAN_NAME;
 
 @AutoConfiguration(afterName = "org.apache.camel.spring.boot.CamelAutoConfiguration")
+@ImportAutoConfiguration(classes = { CamelAutoConfiguration.class })
 @AutoConfigureOrder
 @ConditionalOnClass(CamelContext.class)
 @ConditionalOnExpression("${embedded.containers.enabled:true}")
