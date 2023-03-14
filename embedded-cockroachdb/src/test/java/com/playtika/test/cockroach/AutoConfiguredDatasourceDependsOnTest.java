dc6c3f98e7348a84586cbcaf4c23b3bf3bb0daec
Igor Yova <Igor.Yova@gmail.com>
2023-03-14 11:26:35 +0100

    Add basic support of CockroachDB (#1178)

---
+++ embedded-cockroachdb/src/test/java/com/playtika/test/cockroach/AutoConfiguredDatasourceDependsOnTest.java
@@ -0,0 +1,61 @@
+package com.playtika.test.cockroach;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+import static com.playtika.test.cockroach.CockroachDBProperties.BEAN_NAME_EMBEDDED_COCKROACHDB;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest(
+        classes = AutoConfiguredDatasourceDependsOnTest.TestConfiguration.class,
+        properties = {
+                "spring.profiles.active=enabled",
+                "embedded.toxiproxy.proxies.cockroach.enabled=true"
+        }
+)
+public class AutoConfiguredDatasourceDependsOnTest {
+
+    @Autowired
+    ConfigurableListableBeanFactory beanFactory;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void shouldConnectToCockroachDB() throws Exception {
+        assertThat(jdbcTemplate.queryForObject("select version()", String.class)).contains("CockroachDB");
+    }
+
+    @Test
+    public void shouldSetupDependsOnForAllDataSources() throws Exception {
+        String[] beanNamesForType = BeanFactoryUtils.beanNamesForTypeIncludingAncestors(beanFactory, DataSource.class);
+        assertThat(beanNamesForType)
+                .as("Auto-configured datasource should be present")
+                .hasSize(1)
+                .contains("dataSource");
+        asList(beanNamesForType).forEach(this::hasDependsOn);
+    }
+
+    private void hasDependsOn(String beanName) {
+        assertThat(beanFactory.getBeanDefinition(beanName).getDependsOn())
+                .isNotNull()
+                .isNotEmpty()
+                .contains(BEAN_NAME_EMBEDDED_COCKROACHDB);
+    }
+
+    @EnableAutoConfiguration
+    @Configuration
+    static class TestConfiguration {
+    }
+}
