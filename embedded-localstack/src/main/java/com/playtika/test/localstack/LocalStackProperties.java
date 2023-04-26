745b7dc60b5e786f26b4256bd4b83abb34e94051
Igor Yova <Igor.Yova@gmail.com>
2023-04-26 17:31:19 +0200

    Update localstack/localstack Docker tag to v0.14.5 (#1239)
    * Update localstack/localstack Docker tag to v0.14.5
    
    | datasource | package               | from   | to     |
    | ---------- | --------------------- | ------ | ------ |
    | docker     | localstack/localstack | 0.14.0 | 0.14.5 |
    
    * fix test after upgrade docker images in LocalStack
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- embedded-localstack/src/main/java/com/playtika/test/localstack/LocalStackProperties.java
+++ embedded-localstack/src/main/java/com/playtika/test/localstack/LocalStackProperties.java
@@ -16,14 +16,14 @@ public class LocalStackProperties extends CommonContainerProperties {
     public static final String BEAN_NAME_EMBEDDED_LOCALSTACK = "embeddedLocalstack";
     public Collection<LocalStackContainer.Service> services = Collections.emptyList();
     public int edgePort = 4566;
-    public String hostname = "localhost";
-    public String hostnameExternal = "localhost";
+    public String hostname = "127.0.0.1";
+    public String hostnameExternal = "127.0.0.1";
 
     // https://hub.docker.com/r/localstack/localstack
     @Override
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "localstack/localstack:0.14.0";
+        return "localstack/localstack:0.14.5";
     }
 }
