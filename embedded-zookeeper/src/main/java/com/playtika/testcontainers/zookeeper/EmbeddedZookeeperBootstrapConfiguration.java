0606ef338c6b1f3c79bfb46b1de697361f1ba53f
Vladislav Sidorovich <fenix900@gmail.com>
2023-06-01 23:40:37 +0200

    Add embedded zookeeper

---
+++ embedded-zookeeper/src/main/java/com/playtika/testcontainers/zookeeper/EmbeddedZookeeperBootstrapConfiguration.java
@@ -0,0 +1,71 @@
+package com.playtika.testcontainers.zookeeper;
+
+import com.playtika.testcontainer.common.spring.DockerPresenceBootstrapConfiguration;
+import com.playtika.testcontainer.common.utils.ContainerUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
+
+import java.util.LinkedHashMap;
+import java.util.Optional;
+
+import static com.playtika.testcontainer.common.utils.ContainerUtils.configureCommonsAndStart;
+
+@Slf4j
+@Configuration
+@ConditionalOnExpression("${embedded.containers.enabled:true}")
+@AutoConfigureAfter(DockerPresenceBootstrapConfiguration.class)
+@ConditionalOnProperty(name = "embedded.zookeeper.enabled", havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties(ZookeeperConfigurationProperties.class)
+public class EmbeddedZookeeperBootstrapConfiguration {
+
+    @Bean(destroyMethod = "stop")
+    public GenericContainer<?> zooKeeperContainer(ConfigurableEnvironment environment,
+                                         ZookeeperConfigurationProperties properties,
+                                         Optional<Network> network) {
+        WaitStrategy waitStrategy = new WaitAllStrategy()
+                .withStrategy(new HostPortWaitStrategy())
+                .withStrategy(Wait.forHttp("/commands/ruok")
+                        .forPort(properties.adminServerPort)
+                        .forStatusCode(200)
+                )
+                .withStartupTimeout(properties.getTimeoutDuration());
+        GenericContainer<?> zookeeper = new GenericContainer<>(ContainerUtils.getDockerImageName(properties))
+                .withExposedPorts(properties.clientPort, properties.adminServerPort)
+                .withEnv("ZOO_ADMINSERVER_ENABLED", String.valueOf(true))
+                .waitingFor(waitStrategy);
+        network.ifPresent(zookeeper::withNetwork);
+
+        zookeeper = configureCommonsAndStart(zookeeper, properties, log);
+        registerZookeeperEnvironment(zookeeper, environment, properties);
+
+        return zookeeper;
+    }
+
+    private void registerZookeeperEnvironment(GenericContainer<?> zookeeper, ConfigurableEnvironment environment,
+                                              ZookeeperConfigurationProperties properties) {
+        String host = zookeeper.getHost();
+
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("embedded.zookeeper.port", zookeeper.getMappedPort(properties.clientPort));
+        map.put("embedded.zookeeper.admin.port", zookeeper.getMappedPort(properties.adminServerPort));
+        map.put("embedded.zookeeper.host", host);
+
+        log.info("Started Zookeeper. Connection Details: {}", map);
+
+        MapPropertySource propertySource = new MapPropertySource("embeddedZookeeperInfo", map);
+        environment.getPropertySources().addFirst(propertySource);
+    }
+}
