45d24b2faea3d2785ee431c680a94df41246d180
Igor Yova <Igor.Yova@gmail.com>
2023-08-04 12:29:15 +0200

    Update cockroachdb/cockroach Docker tag to v22.2.12 (#1406)
    | datasource | package               | from    | to       |
    | ---------- | --------------------- | ------- | -------- |
    | docker     | cockroachdb/cockroach | v22.2.9 | v22.2.12 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-cockroachdb/src/main/java/com/playtika/testcontainer/cockroach/CockroachDBProperties.java
+++ embedded-cockroachdb/src/main/java/com/playtika/testcontainer/cockroach/CockroachDBProperties.java
@@ -19,6 +19,6 @@ public class CockroachDBProperties extends CommonContainerProperties {
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "cockroachdb/cockroach:v22.2.9";
+        return "cockroachdb/cockroach:v22.2.12";
     }
 }
