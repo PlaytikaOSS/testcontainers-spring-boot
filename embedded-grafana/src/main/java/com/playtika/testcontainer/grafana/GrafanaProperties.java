0fc566e2e268e0c4e27fbef8c0e341ebf15191f7
Igor Yova <Igor.Yova@gmail.com>
2023-09-26 13:31:41 +0200

    chore(deps): update grafana/grafana docker tag to v10.1.2 (#1508)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-grafana/src/main/java/com/playtika/testcontainer/grafana/GrafanaProperties.java
+++ embedded-grafana/src/main/java/com/playtika/testcontainer/grafana/GrafanaProperties.java
@@ -23,6 +23,6 @@ public class GrafanaProperties extends CommonContainerProperties {
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "grafana/grafana:10.1.1";
+        return "grafana/grafana:10.1.2";
     }
 }
