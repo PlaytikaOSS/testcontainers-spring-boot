252182bef18298c1f5b49415cbe8491836c143ae
ymombrun <ymombrun@users.noreply.github.com>
2023-10-13 10:18:45 +0200

    feat(embedded-opensearch): Allow start with security plugin (#1538)
    Co-authored-by: Yann Mombrun <yann.mombrun@ollca.com>

--- embedded-opensearch/src/main/java/com/playtika/testcontainer/opensearch/rest/CreateIndex.java
+++ embedded-opensearch/src/main/java/com/playtika/testcontainer/opensearch/rest/CreateIndex.java
@@ -4,6 +4,9 @@ import com.playtika.testcontainer.common.checks.AbstractInitOnStartupStrategy;
 import com.playtika.testcontainer.opensearch.OpenSearchProperties;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 public class CreateIndex extends AbstractInitOnStartupStrategy {
 
@@ -12,9 +15,17 @@ public class CreateIndex extends AbstractInitOnStartupStrategy {
 
     @Override
     public String[] getScriptToExecute() {
-        return new String[]{
-            "curl", "-X", "PUT",
-            "http://127.0.0.1:" + properties.getHttpPort() + "/" + index
-        };
+        List<String> command = new ArrayList<>(List.of(
+                "curl", "-X", "PUT", (properties.isCredentialsEnabled() ? "https" : "http") +
+                        "://127.0.0.1:" + properties.getHttpPort() + "/" + index
+        ));
+        if (properties.isCredentialsEnabled()) {
+            command.add("-u");
+            command.add(properties.getUsername() + ':' + properties.getPassword());
+            if (properties.isAllowInsecure()) {
+                command.add("-k");
+            }
+        }
+        return command.toArray(new String[0]);
     }
 }
