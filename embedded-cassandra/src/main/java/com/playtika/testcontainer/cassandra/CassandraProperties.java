746b4da177d85a6db5be0d73973d8443eac89f24
Igor Yova <Igor.Yova@gmail.com>
2023-10-13 14:56:57 +0200

    chore(deps): update cassandra docker tag to v5 (#1553)
    | datasource | package   | from | to  |
    | ---------- | --------- | ---- | --- |
    | docker     | cassandra | 4.1  | 5.0 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-cassandra/src/main/java/com/playtika/testcontainer/cassandra/CassandraProperties.java
+++ embedded-cassandra/src/main/java/com/playtika/testcontainer/cassandra/CassandraProperties.java
@@ -22,6 +22,6 @@ public class CassandraProperties extends CommonContainerProperties {
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "cassandra:4.1";
+        return "cassandra:5.0";
     }
 }
