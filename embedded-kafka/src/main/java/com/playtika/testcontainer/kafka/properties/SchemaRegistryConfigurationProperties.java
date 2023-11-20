3a4edf808ca3c6875f925ac3d564eaf4d68f4fc8
Igor Yova <Igor.Yova@gmail.com>
2023-11-20 12:32:52 +0100

    chore(deps): update confluentinc/cp-schema-registry docker tag to v7.5.2 (#1606)
    | datasource | package                         | from  | to    |
    | ---------- | ------------------------------- | ----- | ----- |
    | docker     | confluentinc/cp-schema-registry | 7.5.1 | 7.5.2 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-kafka/src/main/java/com/playtika/testcontainer/kafka/properties/SchemaRegistryConfigurationProperties.java
+++ embedded-kafka/src/main/java/com/playtika/testcontainer/kafka/properties/SchemaRegistryConfigurationProperties.java
@@ -41,7 +41,7 @@ public class SchemaRegistryConfigurationProperties extends CommonContainerProper
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "confluentinc/cp-schema-registry:7.5.1";
+        return "confluentinc/cp-schema-registry:7.5.2";
     }
 
     public enum AvroCompatibilityLevel {
