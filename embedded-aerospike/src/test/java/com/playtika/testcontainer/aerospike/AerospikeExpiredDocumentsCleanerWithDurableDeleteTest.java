b61ce1ced8302792d0671422071c31f0116b152e
agrgr <agrgr@users.noreply.github.com>
2024-01-21 13:17:31 +0200

    use IAerospikeClient interface, code format

--- embedded-aerospike/src/test/java/com/playtika/testcontainer/aerospike/AerospikeExpiredDocumentsCleanerWithDurableDeleteTest.java
+++ embedded-aerospike/src/test/java/com/playtika/testcontainer/aerospike/AerospikeExpiredDocumentsCleanerWithDurableDeleteTest.java
@@ -1,8 +1,8 @@
 package com.playtika.testcontainer.aerospike;
 
 import com.aerospike.client.AerospikeClient;
-import com.aerospike.client.IAerospikeClient;
 import com.aerospike.client.Bin;
+import com.aerospike.client.IAerospikeClient;
 import com.aerospike.client.Key;
 import com.aerospike.client.policy.ClientPolicy;
 import com.aerospike.client.policy.WritePolicy;
