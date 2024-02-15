bae8e48c2178e9e1637b7acb6351a0661b68f37c
Igor Yova <Igor.Yova@gmail.com>
2024-02-15 17:51:35 +0100

    chore(deps): update clickhouse/clickhouse-server docker tag to v23.12.4 (#1767)
    | datasource | package                      | from    | to      |
    | ---------- | ---------------------------- | ------- | ------- |
    | docker     | clickhouse/clickhouse-server | 23.12.2 | 23.12.4 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-clickhouse/src/main/java/com/playtika/testcontainer/clickhouse/ClickHouseProperties.java
+++ embedded-clickhouse/src/main/java/com/playtika/testcontainer/clickhouse/ClickHouseProperties.java
@@ -24,6 +24,6 @@ public class ClickHouseProperties extends CommonContainerProperties {
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "clickhouse/clickhouse-server:23.12.2";
+        return "clickhouse/clickhouse-server:23.12.4";
     }
 }
