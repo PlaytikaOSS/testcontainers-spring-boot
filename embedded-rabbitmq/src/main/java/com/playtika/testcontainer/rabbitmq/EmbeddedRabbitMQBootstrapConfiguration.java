177c82ecdcdebaae993f22a687b4a152bf47c96d
Ryan R <rriley.ca@gmail.com>
2025-02-06 13:06:39 -0500

    Enabled Rabbit Streams by adding ability to expose additional ports (#2234)
    * Enabled Rabbit Streams by adding ability to expose additional ports
    
    * Convert properties to YAML

--- embedded-rabbitmq/src/main/java/com/playtika/testcontainer/rabbitmq/EmbeddedRabbitMQBootstrapConfiguration.java
+++ embedded-rabbitmq/src/main/java/com/playtika/testcontainer/rabbitmq/EmbeddedRabbitMQBootstrapConfiguration.java
@@ -20,6 +20,7 @@ import org.testcontainers.containers.ToxiproxyContainer;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.playtika.testcontainer.common.utils.ContainerUtils.configureCommonsAndStart;
 import static com.playtika.testcontainer.rabbitmq.RabbitMQProperties.BEAN_NAME_EMBEDDED_RABBITMQ;
@@ -58,11 +59,16 @@ public class EmbeddedRabbitMQBootstrapConfiguration {
     public RabbitMQContainer rabbitmq(ConfigurableEnvironment environment,
                                       RabbitMQProperties properties,
                                       Optional<Network> network) {
+
+        Integer[] exposedPorts = Stream.concat(properties.getAdditionalPorts().stream(), Stream.of(properties.getPort(), properties.getHttpPort()))
+                .distinct()
+                .toArray(Integer[]::new);
+
         RabbitMQContainer rabbitMQ =
                 new RabbitMQContainer(ContainerUtils.getDockerImageName(properties))
                         .withAdminPassword(properties.getPassword())
                         .withEnv("RABBITMQ_DEFAULT_VHOST", properties.getVhost())
-                        .withExposedPorts(properties.getPort(), properties.getHttpPort())
+                        .withExposedPorts(exposedPorts)
                         .withNetworkAliases(RABBITMQ_NETWORK_ALIAS);
 
         if (properties.getEnabledPlugins() != null && properties.getEnabledPlugins().size() != 0) {
@@ -80,6 +86,12 @@ public class EmbeddedRabbitMQBootstrapConfiguration {
                                              RabbitMQProperties properties) {
         Integer mappedPort = rabbitMQ.getMappedPort(properties.getPort());
         Integer mappedHttpPort = rabbitMQ.getMappedPort(properties.getHttpPort());
+
+        Map<Integer, Integer> additionalPorts = new LinkedHashMap<>();
+        for (Integer port : properties.getAdditionalPorts()) {
+            additionalPorts.put(port, rabbitMQ.getMappedPort(port));
+        }
+
         String host = rabbitMQ.getHost();
 
         LinkedHashMap<String, Object> map = new LinkedHashMap<>();
@@ -92,6 +104,9 @@ public class EmbeddedRabbitMQBootstrapConfiguration {
         map.put("embedded.rabbitmq.networkAlias", RABBITMQ_NETWORK_ALIAS);
         map.put("embedded.rabbitmq.internalPort", properties.getPort());
         map.put("embedded.rabbitmq.internalHttpPort", properties.getHttpPort());
+        for (Integer port : additionalPorts.keySet()) {
+            map.put("embedded.rabbitmq.additionalPorts." + port, additionalPorts.get(port));
+        }
 
         log.info("Started RabbitMQ server. Connection details: {}", map);
 
