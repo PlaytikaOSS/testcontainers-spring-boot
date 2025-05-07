de922dc10680bccca2360efabccc4d0b1606d4cc
Igor Yova <Igor.Yova@gmail.com>
2025-05-07 19:20:31 +0200

    chore(deps): update redis docker tag to v8 (#2372)
    | datasource | package | from  | to    |
    | ---------- | ------- | ----- | ----- |
    | docker     | redis   | 7.4.3 | 8.0.0 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-redis/src/main/java/com/playtika/testcontainer/redis/RedisProperties.java
+++ embedded-redis/src/main/java/com/playtika/testcontainer/redis/RedisProperties.java
@@ -35,6 +35,6 @@ public class RedisProperties extends CommonContainerProperties implements Initia
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "redis:7.4.3-alpine";
+        return "redis:8.0.0-alpine";
     }
 }
