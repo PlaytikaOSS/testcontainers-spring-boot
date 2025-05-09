c07a51fadd0a93e1104a4f866545d70cea6907e7
Stanislav Dobrovolschi <st.dobrovolschi@gmail.com>
2025-05-09 11:37:54 +0300

    enable Spring Booot's default CouchbaseAutoConfiguration

--- embedded-couchbase/src/test/java/com/playtika/testcontainer/couchbase/EmbeddedCouchbaseBootstrapConfigurationTest.java
+++ embedded-couchbase/src/test/java/com/playtika/testcontainer/couchbase/EmbeddedCouchbaseBootstrapConfigurationTest.java
@@ -1,18 +1,18 @@
 package com.playtika.testcontainer.couchbase;
 
-import com.playtika.testcontainer.couchbase.springdata.CouchbaseConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
-        classes = {CouchbaseConfiguration.class},
+        classes = {EmbeddedCouchbaseBootstrapConfigurationTest.TestConfiguration.class},
         properties = {
                 "spring.profiles.active=enabled",
-                "spring.main.allow-bean-definition-overriding=true",
                 "embedded.toxiproxy.proxies.couchbase.enabled=true"
         })
 public abstract class EmbeddedCouchbaseBootstrapConfigurationTest {
@@ -30,5 +30,10 @@ public abstract class EmbeddedCouchbaseBootstrapConfigurationTest {
         assertThat(environment.getProperty("embedded.couchbase.password")).isNotEmpty();
         assertThat(System.getProperty("com.couchbase.bootstrapHttpDirectPort")).isNotEmpty();
         assertThat(System.getProperty("com.couchbase.bootstrapCarrierDirectPort")).isNotEmpty();
+    }
+
+    @EnableAutoConfiguration
+    @Configuration
+    static class TestConfiguration {
     }
 }
