dbbf9f4b42b3e571c911d73e3c87f053104e4244
Igor Yova <Igor.Yova@gmail.com>
2025-05-22 10:41:26 +0200

    chore(deps): update authzed/spicedb docker tag to v1.44.0 (#2389)
    | datasource | package         | from    | to      |
    | ---------- | --------------- | ------- | ------- |
    | docker     | authzed/spicedb | v1.43.0 | v1.44.0 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-spicedb/src/main/java/com/playtika/testcontainer/spicedb/SpiceDBProperties.java
+++ embedded-spicedb/src/main/java/com/playtika/testcontainer/spicedb/SpiceDBProperties.java
@@ -19,6 +19,6 @@ public class SpiceDBProperties extends CommonContainerProperties {
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "authzed/spicedb:v1.43.0";
+        return "authzed/spicedb:v1.44.0";
     }
 }
