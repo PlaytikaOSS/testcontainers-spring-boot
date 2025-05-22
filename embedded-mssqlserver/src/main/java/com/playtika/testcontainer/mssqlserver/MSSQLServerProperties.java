1737598696cf60e6e9f43eb95160666842273248
Igor Yova <Igor.Yova@gmail.com>
2025-05-22 10:41:14 +0200

    chore(deps): update mcr.microsoft.com/mssql/server docker tag to v2025 (#2392)
    | datasource | package                        | from | to   |
    | ---------- | ------------------------------ | ---- | ---- |
    | docker     | mcr.microsoft.com/mssql/server | 2022 | 2025 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-mssqlserver/src/main/java/com/playtika/testcontainer/mssqlserver/MSSQLServerProperties.java
+++ embedded-mssqlserver/src/main/java/com/playtika/testcontainer/mssqlserver/MSSQLServerProperties.java
@@ -25,6 +25,6 @@ public class MSSQLServerProperties extends CommonContainerProperties {
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "mcr.microsoft.com/mssql/server:2022-latest";
+        return "mcr.microsoft.com/mssql/server:2025-latest";
     }
 }
