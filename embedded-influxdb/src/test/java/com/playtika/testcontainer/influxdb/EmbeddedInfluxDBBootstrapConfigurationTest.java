af192b981a9506d20c32dac2abe2a98b00c3e083
admitrov <admitrov@playtika.com>
2025-09-24 16:31:48 +0200

    fix: remove unused Spring extension and refactor InfluxDB properties usage in tests

--- embedded-influxdb/src/test/java/com/playtika/testcontainer/influxdb/EmbeddedInfluxDBBootstrapConfigurationTest.java
+++ embedded-influxdb/src/test/java/com/playtika/testcontainer/influxdb/EmbeddedInfluxDBBootstrapConfigurationTest.java
@@ -4,23 +4,17 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class EmbeddedInfluxDBBootstrapConfigurationTest {
 
-    @Autowired
-    private InfluxDBProperties influxDBProperties;
 
     @Value("${embedded.influxdb.port}")
     String influxdbPort;
@@ -49,7 +43,8 @@ class EmbeddedInfluxDBBootstrapConfigurationTest {
     @Test
     void influxDatabaseIsAvailable() {
         RestAssured.baseURI = getUrl();
-        ExtractableResponse response = given()
+        ExtractableResponse<?> response = given()
+                .auth().basic(influxdbUser, influxdbPassword)
                 .get("/ping")
                 .then()
                 .statusCode(HttpStatus.SC_NO_CONTENT)
@@ -59,7 +54,7 @@ class EmbeddedInfluxDBBootstrapConfigurationTest {
     }
 
     private String getUrl() {
-        return "http://" + influxDBProperties.getHost() + ":" + influxDBProperties.getPort();
+        return "http://" + influxdbHost + ":" + influxdbPort;
     }
 
     @EnableAutoConfiguration
