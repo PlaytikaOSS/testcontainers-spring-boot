ffbef8dfdb835bd86113002177b8cc8e37d01dba
Igor Yova <Igor.Yova@gmail.com>
2025-11-03 19:19:31 +0100

    chore(deps): update docker.elastic.co/elasticsearch/elasticsearch docker tag to v9.2.0 (#2646)
    | datasource | package                                       | from  | to    |
    | ---------- | --------------------------------------------- | ----- | ----- |
    | docker     | docker.elastic.co/elasticsearch/elasticsearch | 9.1.5 | 9.2.0 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- embedded-elasticsearch/src/main/java/com/playtika/testcontainer/elasticsearch/ElasticSearchProperties.java
+++ embedded-elasticsearch/src/main/java/com/playtika/testcontainer/elasticsearch/ElasticSearchProperties.java
@@ -26,6 +26,6 @@ public class ElasticSearchProperties extends CommonContainerProperties {
     public String getDefaultDockerImage() {
         // Please don`t remove this comment.
         // renovate: datasource=docker
-        return "docker.elastic.co/elasticsearch/elasticsearch:9.1.5";
+        return "docker.elastic.co/elasticsearch/elasticsearch:9.2.0";
     }
 }
