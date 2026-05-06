9e6f094a03e846c7d18869dd53b8cfca82d5a694
admitrov <admitrov@playtika.com>
2026-05-06 19:10:41 +0100

    Migrate embedded-keycloak to use dasniko/testcontainers-keycloak library
    Replace custom KeycloakContainer and KeycloakContainerFactory implementations with dasniko/testcontainers-keycloak dependency. Consolidate container configuration logic into EmbeddedKeycloakBootstrapConfiguration and refactor tests to use ApplicationContextRunner.

--- embedded-keycloak/src/main/java/com/playtika/testcontainer/keycloak/EmbeddedKeycloakBootstrapConfiguration.java
+++ embedded-keycloak/src/main/java/com/playtika/testcontainer/keycloak/EmbeddedKeycloakBootstrapConfiguration.java
@@ -1,9 +1,11 @@
 package com.playtika.testcontainer.keycloak;
 
 import com.playtika.testcontainer.common.spring.DockerPresenceBootstrapConfiguration;
+import com.playtika.testcontainer.common.utils.ContainerUtils;
 import com.playtika.testcontainer.toxiproxy.ToxiproxyClientProxy;
 import com.playtika.testcontainer.toxiproxy.ToxiproxyHelper;
 import com.playtika.testcontainer.toxiproxy.condition.ConditionalOnToxiProxyEnabled;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import eu.rekawek.toxiproxy.ToxiproxyClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,14 +16,18 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.testcontainers.containers.Network;
 import org.testcontainers.toxiproxy.ToxiproxyContainer;
 
+import java.util.LinkedHashMap;
 import java.util.Optional;
 
+import static com.playtika.testcontainer.common.utils.ContainerUtils.configureCommonsAndStart;
 import static com.playtika.testcontainer.keycloak.KeycloakProperties.BEAN_NAME_EMBEDDED_KEYCLOAK;
-import static java.util.Objects.requireNonNull;
+import static java.lang.String.format;
 
 @Slf4j
 @Configuration
@@ -31,13 +37,16 @@ import static java.util.Objects.requireNonNull;
 @ConditionalOnProperty(name = "embedded.keycloak.enabled", matchIfMissing = true)
 public class EmbeddedKeycloakBootstrapConfiguration {
 
+    private static final String KEYCLOAK_NETWORK_ALIAS = "keycloak.testcontainer.docker";
+    private static final int KEYCLOAK_INTERNAL_HTTP_PORT = 8080;
+
     @Bean
     @ConditionalOnToxiProxyEnabled(module = "keycloak")
     ToxiproxyClientProxy keycloakContainerProxy(ToxiproxyClient toxiproxyClient,
-                                                 ToxiproxyContainer toxiproxyContainer,
-                                                 @Qualifier(BEAN_NAME_EMBEDDED_KEYCLOAK) KeycloakContainer keycloakContainer,
-                                                 KeycloakProperties properties,
-                                                 ConfigurableEnvironment environment) {
+                                                ToxiproxyContainer toxiproxyContainer,
+                                                @Qualifier(BEAN_NAME_EMBEDDED_KEYCLOAK) KeycloakContainer keycloakContainer,
+                                                KeycloakProperties properties,
+                                                ConfigurableEnvironment environment) {
         ToxiproxyClientProxy proxy = ToxiproxyHelper.createProxy(
                 toxiproxyClient,
                 toxiproxyContainer,
@@ -50,26 +59,105 @@ public class EmbeddedKeycloakBootstrapConfiguration {
         return proxy;
     }
 
-    @Bean
-    public KeycloakContainerFactory keycloakContainerFactory(ConfigurableEnvironment environment,
-                                                             KeycloakProperties properties,
-                                                             ResourceLoader resourceLoader,
-                                                             Optional<Network> network) {
-        return new KeycloakContainerFactory(environment, properties, resourceLoader, network);
+    @Bean(name = BEAN_NAME_EMBEDDED_KEYCLOAK, destroyMethod = "stop")
+    public KeycloakContainer keycloakContainer(ConfigurableEnvironment environment,
+                                               KeycloakProperties properties,
+                                               ResourceLoader resourceLoader,
+                                               Optional<Network> network) {
+        KeycloakContainer keycloak = new KeycloakContainer(ContainerUtils.getDockerImageName(properties).toString())
+                .withNetworkAliases(KEYCLOAK_NETWORK_ALIAS)
+                .withAdminUsername(properties.getAdminUser())
+                .withAdminPassword(properties.getAdminPassword());
+
+        applyDbConfig(keycloak, properties);
+        applyImportFile(keycloak, properties, resourceLoader);
+
+        network.ifPresent(keycloak::withNetwork);
+
+        keycloak = (KeycloakContainer) configureCommonsAndStart(keycloak, properties, log);
+        registerEnvironment(keycloak, environment, properties);
+        return keycloak;
     }
 
-    /**
-     * Creates and starts a {@link KeycloakContainer} if property {@code embedded.keycloak.enabled}
-     * evaluates to {@code true}. The configuration makes no difference if just vanilla Keycloak is
-     * on the classpath or any Spring adapter. The container will always be needed. Also registers a
-     * shutdown hook to stop the container on context shutdown.
-     *
-     * @param factory The {@link KeycloakContainerFactory} to use, injected by Spring, must not be
-     *                null
-     * @return The created {@link KeycloakContainer} instance to be registered as bean
-     */
-    @Bean(name = BEAN_NAME_EMBEDDED_KEYCLOAK, destroyMethod = "stop")
-    public KeycloakContainer keycloakContainer(KeycloakContainerFactory factory) {
-        return requireNonNull(factory).newKeycloakContainer();
+    private void applyDbConfig(KeycloakContainer keycloak, KeycloakProperties properties) {
+        if (properties.getDbVendor() != null) {
+            keycloak.withEnv("DB", properties.getDbVendor());
+        }
+        if (properties.getDbAddr() != null) {
+            keycloak.withEnv("DB_URL_HOST", properties.getDbAddr());
+        }
+        if (properties.getDbPort() != null) {
+            keycloak.withEnv("DB_URL_PORT", properties.getDbPort());
+        }
+        if (properties.getDbDatabase() != null) {
+            keycloak.withEnv("DB_URL_DATABASE", properties.getDbDatabase());
+        }
+        if (properties.getDbSchema() != null) {
+            keycloak.withEnv("DB_SCHEMA", properties.getDbSchema());
+        }
+        if (properties.getDbUser() != null) {
+            keycloak.withEnv("DB_USERNAME", properties.getDbUser());
+        }
+        if (properties.getDbUserFile() != null) {
+            keycloak.withEnv("DB_USER_FILE", properties.getDbUserFile());
+        }
+        if (properties.getDbPassword() != null) {
+            keycloak.withEnv("DB_PASSWORD", properties.getDbPassword());
+        }
+        if (properties.getDbPasswordFile() != null) {
+            keycloak.withEnv("DB_PASSWORD_FILE", properties.getDbPasswordFile());
+        }
+    }
+
+    private void applyImportFile(KeycloakContainer keycloak, KeycloakProperties properties, ResourceLoader resourceLoader) {
+        String importFile = properties.getImportFile();
+        if (importFile == null) {
+            return;
+        }
+        checkImportFileExists(resourceLoader, importFile);
+        keycloak.withRealmImportFile(importFile);
+    }
+
+    private void checkImportFileExists(ResourceLoader resourceLoader, String importFile) {
+        Resource resource = resourceLoader.getResource("classpath:" + importFile);
+        if (!resource.exists()) {
+            throw new ImportFileNotFoundException(importFile);
+        }
+        log.debug("Using import file: {}", resource.getFilename());
+    }
+
+    private void registerEnvironment(KeycloakContainer keycloak,
+                                     ConfigurableEnvironment environment,
+                                     KeycloakProperties properties) {
+        String host = keycloak.getHost();
+        Integer httpPort = keycloak.getHttpPort();
+        String authServerUrl = buildAuthServerUrl(keycloak, properties);
+
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("embedded.keycloak.host", host);
+        map.put("embedded.keycloak.http-port", httpPort);
+        map.put("embedded.keycloak.auth-server-url", authServerUrl);
+        map.put("embedded.keycloak.networkAlias", KEYCLOAK_NETWORK_ALIAS);
+        map.put("embedded.keycloak.internalPort", KEYCLOAK_INTERNAL_HTTP_PORT);
+
+        log.info("Started Keycloak server. Connection details: {}", map);
+
+        MapPropertySource propertySource = new MapPropertySource("embeddedKeycloakInfo", map);
+        environment.getPropertySources().addFirst(propertySource);
+    }
+
+    private String buildAuthServerUrl(KeycloakContainer keycloak, KeycloakProperties properties) {
+        return format("http://%s:%d%s", keycloak.getHost(), keycloak.getHttpPort(), properties.getAuthBasePath());
+    }
+
+    public static final class ImportFileNotFoundException extends IllegalArgumentException {
+
+        private static final long serialVersionUID = 6350884396691857560L;
+
+        ImportFileNotFoundException(String importFile) {
+            super(format(
+                    "Classpath resource '%s' defined through 'embedded.keycloak.import-file' does not exist.",
+                    importFile));
+        }
     }
 }
--- embedded-keycloak/src/main/java/com/playtika/testcontainer/keycloak/KeycloakProperties.java
+++ embedded-keycloak/src/main/java/com/playtika/testcontainer/keycloak/KeycloakProperties.java
@@ -12,18 +12,12 @@ public class KeycloakProperties extends CommonContainerProperties {
 
     public static final String BEAN_NAME_EMBEDDED_KEYCLOAK = "embeddedKeycloak";
 
-    public static final String[] DEFAULT_COMMAND = {"start-dev", "--import-realm"};
-
     public static final String DEFAULT_ADMIN_USER = "admin";
     public static final String DEFAULT_ADMIN_PASSWORD = "letmein";
     public static final String DEFAULT_REALM = "master";
     public static final String DEFAULT_AUTH_BASE_PATH = "/";
     public static final String DEFAULT_DB_VENDOR = "dev-mem";
 
-    /**
-     * The command string issued to the container.
-     */
-    private String[] command = DEFAULT_COMMAND;
     /**
      * The admin username to use.
      */
@@ -76,6 +70,11 @@ public class KeycloakProperties extends CommonContainerProperties {
      * Specify user's password to use to authenticate to the database via file input (alternative to DB_PASSWORD).
      */
     private String dbPasswordFile;
+
+    @Override
+    public String[] getCommand() {
+        return null;
+    }
 
     @Override
     public String getDefaultDockerImage() {
