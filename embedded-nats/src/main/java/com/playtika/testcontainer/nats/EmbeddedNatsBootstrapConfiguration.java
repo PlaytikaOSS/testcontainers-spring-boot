4f8bdf0b6d7f1f9bc46e94506fadc803a0182e01
admitrov <admitrov@playtika.com>
2026-05-14 11:31:10 +0100

    Replace LoggerFactory with Lombok @Slf4j annotation in EmbeddedNatsBootstrapConfiguration
    Remove manual logger instantiation and use @Slf4j annotation. Remove unused LoggerFactory and Logger imports.

--- embedded-nats/src/main/java/com/playtika/testcontainer/nats/EmbeddedNatsBootstrapConfiguration.java
+++ embedded-nats/src/main/java/com/playtika/testcontainer/nats/EmbeddedNatsBootstrapConfiguration.java
@@ -9,8 +9,6 @@ import com.playtika.testcontainer.toxiproxy.condition.ConditionalOnToxiProxyEnab
 import eu.rekawek.toxiproxy.ToxiproxyClient;
 import io.github.amadeusitgroup.testcontainers.nats.NatsContainer;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
